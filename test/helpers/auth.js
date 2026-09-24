81c6ffeb6b47bc4941c126a867b8c48797d9e90f
Fagner Paes <fagner.paes@bestcode.com.br>
2026-09-24 05:39:35 -0300

    adicionado o login com token admin e utilizado nos testes de Matricula de Aluno em disciplina.

--- test/helpers/auth.js
+++ test/helpers/auth.js
@@ -2,6 +2,22 @@ import { api } from './api.js';
 import request from 'supertest';
 import 'dotenv/config';
 
+
+let tokenEmCache = null
+
+export async function comTokenDeAdmin() {
+    if (!tokenEmCache) {
+        const loginResposta = await api()
+            .post('/api/auth/login')
+            .set('Content-Type', 'application/json')
+            .send({ 'email': process.env.ADMIN_EMAIL, 
+                'senha': process.env.ADMIN_SENHA });
+
+        tokenEmCache = loginResposta.body.token;
+    }
+    return `Bearer ${tokenEmCache}`;
+}
+
 export async function getToken(emailUser, passUser) {
     const loginResposta = await api()
         .post('/api/auth/login')
